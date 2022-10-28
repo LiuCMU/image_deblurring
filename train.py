9d8af11f4099a4cf879c748453cb45ff5dd4e4cf
Zhen (Jack) Liu <liu5@andrew.cmu.edu>
2022-10-27 23:10:26 -0400

    add SSIM & correct scheduler

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import torch
 from torch.utils.data import DataLoader
 from data import img_dataset
 from model import Conv
+from SSIM import ssim
 
 
 """The following parameters are based on:
@@ -53,27 +54,30 @@ def calc_PSNR(img1, img2):
     img1/img1: tensor of shape (N, C, H, W)
     """
     N, C, H, W = img1.shape
-    denominator = (1/(N*C*H*W)) * np.sum(np.square(img1 - img2))
-    psnr = 10*np.log10((255*255)/denominator)
-    return psnr
+    denominator = (1/(N*C*H*W)) * torch.sum(torch.square(img1 - img2))
+    psnr = 10*torch.log10((255*255)/denominator)
+    return psnr.item()
 
 
 def validate(loader):
     model.eval()
     with torch.no_grad():
-        psnrs, sizes = [], []
+        psnrs, ssims, sizes = [], [], []
         for batch in loader:
             xs_, ys_ = batch
             xs_ = xs_.to(device)
             ys_ = ys_.to(device)
-            yhat = model(xs_).detach().to("cpu").numpy()
-            ys = ys_.detach().to("cpu").numpy()
+            yhat = model(xs_).detach()
+            ys = ys_.detach()
             psnr = calc_PSNR(yhat, ys)
+            ssim_i = ssim(yhat, ys).item()
 
             psnrs.append(psnr)
+            ssims.append(ssim_i)
             sizes.append(len(batch))
     score = np.dot(psnrs, sizes)/np.sum(sizes)
-    return score
+    ssim_score = np.dot(ssims, sizes)/np.sum(sizes)
+    return (score, ssim_score)
 
 
 def init_params(m):
@@ -83,23 +87,22 @@ def init_params(m):
 
 #load datasets
 train = img_dataset(train_path, debug=False, scale=True)
-train_loader = DataLoader(train, batchsize, num_workers=6)
+train_loader = DataLoader(train, batchsize, num_workers=4)
 test = img_dataset(test_path, debug=False, scale=True)
-test_loader = DataLoader(test, batchsize, num_workers=6)
+test_loader = DataLoader(test, batchsize, num_workers=4)
 
 model = Conv(config.num_layers, config.conv_pad, config.hidden_channels, config.pool_pad)
 model.to(device)
 model.apply(init_params)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=patience, mode="min")
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=patience, mode="max")
 print("Total number of trainable parameters: ", sum(p.numel() for p in model.parameters() if p.requires_grad))
 
-nspr = 999
 results = []
 for i in range(epochs):
     epoch = i+1
     learning_rate = optimizer.param_groups[0]['lr']
-    scheduler.step(nspr)
+    
 
     model.train()
     Yhats, Ys = [], []
@@ -114,16 +117,20 @@ for i in range(epochs):
         loss.backward()
         optimizer.step()
 
-    train_psnr = validate(train_loader)
-    test_psnr = validate(test_loader)
-    print("Epoch %i LR: %.4f Training NSPR: %.2f Test NSPR: %.2f" % (epoch, learning_rate,  train_psnr, test_psnr))
+    
+    train_psnr, train_ssim = validate(train_loader)
+    test_psnr, test_ssim = validate(test_loader)
+    print("Epoch %i LR: %.4f Training PSNR: %.2f SSIM: %.2f Test PSNR: %.2f SSIM %.2f" % (epoch, learning_rate,  train_psnr, train_ssim, test_psnr, test_ssim))
     wandb.log({
         "learning_rate": learning_rate,
         "train_psnr": train_psnr,
-        "test_psnr": test_psnr
+        "train_ssim": train_ssim,
+        "test_psnr": test_psnr,
+        "test_ssim": test_ssim
     })
-    results.append((epoch, learning_rate,  train_psnr, test_psnr))
+    results.append((epoch, learning_rate,  train_psnr, train_ssim, test_psnr, test_ssim))
     if i//10 == 0:
         torch.save(model.state_dict(), "latest.pt")
+    scheduler.step(test_psnr)
 torch.save(model.state_dict(), "model_parameters.pt")
 pickle.dump(results, open("results.pkl", "wb"))
