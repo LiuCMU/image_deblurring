abdf0f0b1cb4f01dd2a82a2dbc83ceed9a60ef13
Zhen (Jack) Liu <liu5@andrew.cmu.edu>
2022-11-27 17:29:14 -0500

    add Res

--- model.py
+++ model.py
@@ -43,3 +43,45 @@ class Conv(nn.Module):
         x = self.relu2(self.conv2(x))
         return x
 
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size = 5, stride = 1, padding = 2):
+        super(ResBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size, stride = stride, padding = padding),
+                        nn.BatchNorm2d(out_channels),
+                        nn.ReLU())
+        self.conv2 = nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels, kernel_size, stride = stride, padding = padding),
+                        nn.BatchNorm2d(out_channels))
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+    
+    def forward(self,x):
+        input = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out += input
+        out = self.relu(out)
+        return out
+
+class ResConv(nn.Module):
+    def __init__(self):
+        super(ResConv, self).__init__()
+        self.convStart = nn.Conv2d(3, 20, 5, stride=1, padding=2)
+        self.ResBlock1 = ResBlock(20,20,3,1,1)
+        self.ResBlock2 = ResBlock(20,20,3,1,1)
+        self.ResBlock3 = ResBlock(20,20,3,1,1)
+        self.ResBlock4 = ResBlock(20,20,3,1,1)
+        self.ResBlock5 = ResBlock(20,20,3,1,1)
+        self.convEnd = nn.Conv2d(20, 3, 5, stride=1, padding=2)
+
+    def forward(self,x):
+        out = self.convStart(x)
+        out = self.ResBlock1(out)
+        out = self.ResBlock2(out)
+        out = self.ResBlock3(out)
+        out = self.ResBlock4(out)
+        out = self.ResBlock5(out)
+        out = self.convEnd(out)
+        return out
